57440250d6490b95c11d09d85fb7f07f0057b90e
Miguel <migjaques@gmail.com>
2021-02-18 22:06:25 +0000

    Move remote_setup.sh file to scripts folder

---
+++ scripts/remote_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd /home/ubuntu
+
+echo 'Hello World'
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > /dev/null
+bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/ubuntu/miniconda3
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+export PATH=$PATH:/home/ubuntu/miniconda3/bin
+conda create -y -q -n nimbo python=3.7 
+conda activate nimbo
+conda env update --file nimbo-environment.yml
+conda install -c conda-forge awscli -y
