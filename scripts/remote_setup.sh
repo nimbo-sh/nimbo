082217ebd0e630599651c7b988053ad62cc4899c
Miguel <migjaques@gmail.com>
2021-02-20 00:03:05 +0000

    Small fixes to remote_setup.sh

--- scripts/remote_setup.sh
+++ scripts/remote_setup.sh
@@ -5,8 +5,9 @@ cd /home/ubuntu
 
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/ubuntu/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+echo "source /home/ubuntu/miniconda3/etc/profile.d/conda.sh" >> .bashrc
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
-export PATH=$PATH:/home/ubuntu/miniconda3/bin
 conda create -y -q -n nimbo python=3.7 
 conda activate nimbo
 conda env update -q --file nimbo-environment.yml
