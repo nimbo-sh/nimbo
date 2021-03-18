6c87f1184261dee2774d2e9db2ac0bb5ffc9a571
Miguel <migjaques@gmail.com>
2021-03-18 13:48:58 +0000

    Remove EBS snapshot code and do everything on S3

--- core/launch.py
+++ core/launch.py
@@ -10,29 +10,12 @@ from . import storage
 from . import utils
 
 
-def launch_instance(session, config, noscript=False):
+def launch_instance(session, config, job_cmd, noscript=False):
+    print("Job command:", job_cmd)
 
     # Create main bucket
     # Operation is idempotent, so will not do anything if bucket already exists
     success = storage.create_bucket(session, 'nimbo-main-bucket')
-
-    snapshots = storage.list_snapshots(session)
-
-    if len(snapshots) == 0:
-        print("No conda volume found. Creating new one.")
-        new_volume = 1
-        ebs_dict = {
-            'DeleteOnTermination': True,
-            'VolumeSize': 8,
-            'VolumeType': 'standard',
-        }
-    else:
-        print(f"Creating conda volume from snapshot {snapshots[-1]['SnapshotId']}")
-        new_volume = 0
-        ebs_dict = {
-            'DeleteOnTermination': True,
-            'SnapshotId': snapshots[-1]["SnapshotId"],
-        }
 
     # Launch instance with new volume for anaconda
     print("Launching instance... ", end="", flush=True)
@@ -41,12 +24,6 @@ def launch_instance(session, config, noscript=False):
     # print(userdata)
     start_t = time.time()
     instance = ec2.run_instances(
-        BlockDeviceMappings=[
-            {
-                'DeviceName': '/dev/sdf',
-                'Ebs': ebs_dict,
-            },
-        ],
         ImageId=config['ami'],
         InstanceType=config["instance_type"],
         KeyName=config["instance_key"],
@@ -85,23 +62,27 @@ def launch_instance(session, config, noscript=False):
     print("Ready.")
 
     # Get conda env yml of current env
-    command = "conda env export > nimbo-environment.yml"
+    command = "conda env export > local_env.yml"
     output, error = subprocess.Popen(command, shell=True).communicate()
 
     # Send conda env yaml and setup scripts to instance
     print("\nSyncing conda, config, and setup files...")
-    subprocess.Popen(f"scp -i ./instance-key.pem ./nimbo-environment.yml ./config.yml ./scripts/remote_setup.sh ubuntu@{host}:/home/ubuntu", shell=True).communicate()
-    subprocess.Popen(f"rm ./nimbo-environment.yml", shell=True).communicate()
-
-    print("\nSetting up environment...")
-    command = f"'export NEW_VOLUME={new_volume} && bash remote_setup.sh'"
-    subprocess.Popen(f"ssh -i ./instance-key.pem ubuntu@{host} {command}", shell=True).communicate()
+    subprocess.Popen("scp -i ./instance-key.pem "
+                     "./local_env.yml ./config.yml "
+                     "./scripts/remote_setup.sh "
+                     f"ubuntu@{host}:/home/ubuntu", shell=True).communicate()
+    subprocess.Popen(f"rm ./local_env.yml", shell=True).communicate()
 
     # Sync code with instance
     print("\nSyncing code...")
     subprocess.Popen(f"rsync -avm -e 'ssh -i ./instance-key.pem' "
                      f"--include '*/' --include '*.py' --exclude '*' "
                      f". ubuntu@{host}:/home/ubuntu", shell=True).communicate()
+
+    print("\nSetting up environment...")
+    command = "bash remote_setup.sh"
+    subprocess.Popen(f"ssh -i ./instance-key.pem ubuntu@{host} {command} {job_cmd}", shell=True).communicate()
+
 
     # aws ssm send-command --document-name "AWS-RunShellScript" --comment "listing services" --instance-ids "Instance-ID"
     # --parameters commands="service --status-all" --region us-west-2 --output text
@@ -111,34 +92,6 @@ def launch_instance(session, config, noscript=False):
     else:
         # Run user script
         pass
-
-    # Create snapshot
-    print("Saving conda env in a snapshot...", end="", flush=True)
-    response = ec2.describe_volumes(Filters=[
-        {
-            "Name": "attachment.instance-id",
-            "Values": [instance["InstanceId"]]
-        },
-        {
-            "Name": "attachment.device",
-            "Values": ['/dev/sdf']
-        }
-    ])
-    volume = response["Volumes"][0]
-    volume_id = volume["Attachments"][0]["VolumeId"]
-    snapshot = ec2.create_snapshot(
-        Description="Conda folder snapshot",
-        VolumeId=volume_id,
-        TagSpecifications=[{
-            "ResourceType": "snapshot",
-            "Tags": [{"Key": "created_by", "Value": "nimbo"}]
-        }]
-    )
-    # Wait for snapshot to be completed
-    while storage.check_snapshot_state(session, snapshot["SnapshotId"]) != "completed":
-        time.sleep(2)
-    print("Done.")
-
 
     if config["delete_after_job_finish"] == True:
         # Terminate instance
--- scripts/remote_setup.sh
+++ scripts/remote_setup.sh
@@ -3,47 +3,8 @@ set -e
 
 cd /home/ubuntu
 
-echo "$NEW_VOLUME"
+AWS=/usr/local/bin/aws
 CONDA_PATH=/home/ubuntu/miniconda3
-
-if [ "$NEW_VOLUME" -eq 1 ]; then
-    echo "mkfs -t xfs /dev/xvdf"
-    sudo mkfs -t xfs /dev/xvdf
-fi
-
-mkdir -p $CONDA_PATH
-
-if grep -qs "$CONDA_PATH " /proc/mounts; then
-    echo "$CONDA_PATH already mounted."
-else
-    echo "$CONDA_PATH not mounted. Mounting /dev/xvdf..."
-    sudo mount /dev/xvdf $CONDA_PATH
-fi
-sudo chmod 777 $CONDA_PATH
-
-#sudo apt-get -qq update
-#sudo apt-get -qq install awscli -y
-
-CONDASH=$CONDA_PATH/etc/profile.d/conda.sh
-ENV_NAME="$(grep 'name:' nimbo-environment.yml | awk '{print $2}')"
-echo "Using conda env $ENV_NAME"
-
-if [ -f "$CONDASH" ]; then
-    echo "source $CONDASH" >> .bashrc
-    source $CONDASH
-    conda activate $ENV_NAME
-    conda env update -q --file nimbo-environment.yml
-else
-    wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -bfp /home/ubuntu/miniconda3
-    rm Miniconda3-latest-Linux-x86_64.sh
-    echo "source $CONDASH" >> .bashrc
-    source $CONDASH
-    conda env create -q --file nimbo-environment.yml
-    conda activate $ENV_NAME
-    #conda install -q -c conda-forge awscli -y
-fi
-echo "Conda setup complete."
 
 DATASETS_PATH="$(grep 'datasets_path:' config.yml | awk '{print $2}')"
 RESULTS_PATH="$(grep 'results_path:' config.yml | awk '{print $2}')"
@@ -52,6 +13,53 @@ echo "Datasets path: $DATASETS_PATH"
 echo "Results path: $RESULTS_PATH"
 echo "Bucket name: $BUCKET_NAME"
 
+CONDASH=$CONDA_PATH/etc/profile.d/conda.sh
+ENV_NAME="$(grep 'name:' local_env.yml | awk '{print $2}')"
+echo ""
+echo "Using conda env $ENV_NAME"
+
+# Import conda from s3
+echo ""
+echo "Importing conda from s3..."
+mkdir -p $CONDA_PATH
+$AWS s3 cp s3://$BUCKET_NAME/conda-envs.tar /home/ubuntu/
+tar -xf conda-envs.tar -C $CONDA_PATH
+rm conda-envs.tar
+
+
+if [ -f "$CONDASH" ]; then
+    echo ""
+    echo "Conda installation found."
+    echo "source $CONDASH" >> .bashrc
+    source $CONDASH
+    conda activate $ENV_NAME
+    conda env export > existing_env.yml
+
+    # We compare all the lines except the last one, because that one has the conda profiles,
+    # which will necessarily be different.
+    if cmp -s <(head -n -1 existing_env.yml) <(head -n -1 local_env.yml); then
+        echo "Imported env matches existing env. Skipping updates."
+        UPDATE_ENV=0
+    else
+        echo "Imported env doesn't match existing env. Updating env..."
+        conda env update -q --file local_env.yml
+        UPDATE_ENV=1
+    fi
+    rm existing_env.yml
+else
+    echo "Conda installation not found. Installing..."
+    wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -bfp /home/ubuntu/miniconda3
+    rm Miniconda3-latest-Linux-x86_64.sh
+    echo "source $CONDASH" >> .bashrc
+    source $CONDASH
+    conda env create -q --file local_env.yml
+    conda activate $ENV_NAME
+    UPDATE_ENV=1
+fi
+
+echo "Conda setup complete."
+
 # Import datasets and results from the bucket
 mkdir -p $DATASETS_PATH
 mkdir -p $RESULTS_PATH
@@ -59,9 +67,27 @@ mkdir -p $RESULTS_PATH
 S3_DATASETS_PATH=s3://$BUCKET_NAME/$DATASETS_PATH
 S3_RESULTS_PATH=s3://$BUCKET_NAME/$RESULTS_PATH
 
+echo ""
 echo "Importing datasets from $S3_DATASETS_PATH"
-aws s3 cp --recursive $S3_DATASETS_PATH $DATASETS_PATH
-echo "Importing results from $S3_RESULTS_PATH"
-aws s3 cp --recursive $S3_RESULTS_PATH $RESULTS_PATH
+$AWS s3 cp --recursive $S3_DATASETS_PATH $DATASETS_PATH
+printf "Importing results from $S3_RESULTS_PATH"
+$AWS s3 cp --recursive $S3_RESULTS_PATH $RESULTS_PATH
+
+# If the local and existing environments are different, update the env on the bucket
+if [ $UPDATE_ENV -eq 1 ]; then
+    echo ""
+    echo "Zipping conda env and saving to bucket..."
+    tar -cf /home/ubuntu/conda-envs.tar -C $CONDA_PATH .
+    $AWS s3 cp /home/ubuntu/conda-envs.tar s3://$BUCKET_NAME/
+fi
+
+cd repo
+echo ""
+echo "Running script..."
+printf "$@"
+$@
+cd ..
 
 conda deactivate
+echo ""
+echo "Done."
