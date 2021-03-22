08fca269560c63183975a67f3d665df51429efeb
Miguel <migjaques@gmail.com>
2021-03-22 12:44:16 +0000

    Add new AMIs

--- core/ami/catalog.py
+++ core/ami/catalog.py
@@ -1,5 +1,7 @@
 
 AMI_MAP = {
     "ubuntu18-cuda11.0-cudnn8.0-conda4.9.2": "ami-0189241054cf4869f",
+    "ubuntu18-cuda10.2-cudnn7.6-conda4.9.2": "ami-06a87caa719ccb3f2",
+    "ubuntu18-conda4.9.2": "ami-0b1341587b0d2e5c4",
     "ubuntu18": "ami-0e5657f6d3c3ea350"
 }
