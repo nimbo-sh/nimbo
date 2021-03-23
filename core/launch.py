dab668d8819e3405669adb305ae60d6318e060ab
seuqaj114 <migjaques@gmail.com>
2021-03-23 20:20:52 +0000

    Add command to test access and permissions

--- core/launch.py
+++ core/launch.py
@@ -140,4 +140,93 @@ def run_job(session, config, job_cmd):
     if config["delete_after_job_finish"] == True and \
        job_cmd != "_nimbo_launch_and_setup":
         # Terminate instance
-        utils.delete_instance(session, instance["InstanceId"])
+        utils.delete_instance(session, instance_id)
+
+
+def run_access_test(session, config):
+
+    access.verify_nimbo_instance_profile(session)
+    print("Instance profile 'NimboInstanceProfile' found \u2713")
+
+    # Launch instance with new volume for anaconda
+    print("Launching test instance... ", end="", flush=True)
+    ec2 = session.client('ec2')
+
+    # print(userdata)
+    start_t = time.time()
+    instance_config = {
+        "BlockDeviceMappings": [{
+            'DeviceName': '/dev/sda1',
+            'Ebs': {'VolumeSize': config["disk_size"]}
+        }],
+        "ImageId": AMI_MAP[config['image']],
+        "InstanceType": config["instance_type"],
+        "KeyName": config["instance_key"],
+        "Placement": {"Tenancy": "default"},
+        "SecurityGroups": [config["security_group"]],
+        "IamInstanceProfile": {
+            "Name": "NimboInstanceProfile"
+        }
+    }
+    instance_config["MinCount"] = 1
+    instance_config["MaxCount"] = 1
+    instance = ec2.run_instances(**instance_config)
+    instance = instance["Instances"][0]
+
+    instance_id = instance["InstanceId"]
+    status = ""
+
+    # Wait for the instance to be running
+    while status != "running":
+        time.sleep(1)
+        status = utils.check_instance_status(session, instance_id)
+
+    end_t = time.time()
+    print(f"Instance running. Instance creation allowed \u2713")
+    print()
+
+    INSTANCE_KEY = config["instance_key"]+".pem"
+    host = utils.check_instance_host(session, instance_id)
+    ssh = f"ssh -i {INSTANCE_KEY} -o 'StrictHostKeyChecking no'"
+    scp = f"scp -i {INSTANCE_KEY}"
+
+    # Wait for the instance to be ready for ssh
+    print("Waiting for instance to be ready for ssh... ", end="", flush=True)
+    host_ready = False
+    wait_time = 0
+    time.sleep(30)
+    while 1:
+        output, error = subprocess.Popen(f"{ssh} ubuntu@{host} echo HelloWorld", shell=True,
+                                         stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        wait_time += 5
+        if error == b'':
+            break
+
+        if wait_time == 60:
+            print("\nDeleting test instance...")
+            utils.delete_instance(session, instance_id)
+            raise Exception("Something failed connecting to the instance. "
+                            "Please verify your security groups, instance key and instance profile. "
+                            "More info at docs.nimbo.sh/connection-issues.")
+
+    print("Ready.")
+    print("Instance key allows ssh access to remote instance \u2713")
+    print("Security group allows ssh access to remote instance \u2713")
+
+    CONFIG = "config.yml"
+    REMOTE_TEST = join(NIMBO, "scripts/remote_s3_test.sh")
+    subprocess.Popen(f"{scp} {CONFIG} {REMOTE_TEST} "
+                     f"ubuntu@{host}:/home/ubuntu", shell=True).communicate()
+    command = "bash remote_s3_test.sh"
+    output, error = subprocess.Popen(f"{ssh} ubuntu@{host} {command} {instance_id}", shell=True,
+                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+    if error == b'':
+        print("The instance has access the required S3 and EC2 permissions \u2713")
+        print("\nEverything working \u2713")
+        print("Instance has been deleted.")
+    else:
+        print("Error found:")
+        print(error.decode("utf-8"))
+        print("\nDeleting test instance...")
+        utils.delete_instance(session, instance_id)
