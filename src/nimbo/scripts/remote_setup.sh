7b4569db28968075afdc0ba43d736cd9358e50b4
Miguel <migjaques@gmail.com>
2021-03-27 10:45:46 +0000

    Always install conda env from scratch

--- src/nimbo/scripts/remote_setup.sh
+++ src/nimbo/scripts/remote_setup.sh
@@ -26,60 +26,27 @@ echo "Using conda env: $ENV_NAME"
 # Import conda from s3
 echo ""
 mkdir -p $CONDA_PATH
-if $AWS s3 ls s3://$BUCKET_NAME/conda-envs.tar; then
-    echo "Importing your conda envs from s3..."
-    $AWS s3 cp --quiet s3://$BUCKET_NAME/conda-envs.tar /home/ubuntu/
-    tar -xf /home/ubuntu/conda-envs.tar -C $CONDA_PATH
-    rm /home/ubuntu/conda-envs.tar
-fi
-
 
 # ERROR: This currently doesn't allow for a new unseen env to be passed. Fix this.
 if [ -f "$CONDASH" ]; then
     echo ""
     echo "Conda installation found."
-    #echo "source $CONDASH" >> .bashrc
-    source $CONDASH
-
-    if conda activate $ENV_NAME; then
-        # If env exists
-        echo "Env $ENV_NAME found."
-        conda activate $ENV_NAME
-        conda env export > existing_env.yml
-
-        # We compare all the lines except the last one, because that one has the conda profiles,
-        # which will necessarily be different.
-        if cmp -s <(head -n -1 existing_env.yml) <(head -n -1 local_env.yml); then
-            echo "Imported env matches existing env. Skipping updates."
-            UPDATE_ENV=0
-        else
-            echo "Imported env doesn't match existing env. Updating env..."
-            conda env update -q --file local_env.yml
-            UPDATE_ENV=1
-        fi
-        rm existing_env.yml
-    else
-        # If env doesn't exit
-        echo "Env $ENV_NAME not found. Creating..."
-        time conda env create -q --file local_env.yml
-        conda activate $ENV_NAME
-        UPDATE_ENV=1
-    fi;
-
 else
     echo "Conda installation not found. Installing..."
     wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -bfp /home/ubuntu/miniconda3
     rm Miniconda3-latest-Linux-x86_64.sh
     echo "source $CONDASH" >> .bashrc
-    source $CONDASH
-    echo "Creating $ENV_NAME env..."
-    conda env create -q --file local_env.yml
-    conda activate $ENV_NAME
-    UPDATE_ENV=1
 fi
 
-echo "Conda setup complete."
+source $CONDASH
+
+# If env doesn't exit
+echo "Creating conda environment..."
+time conda env create -q --file local_env.yml
+conda activate $ENV_NAME
+
+echo "Done."
 
 # Import datasets and results from the bucket
 mkdir -p $DATASETS_PATH
@@ -113,19 +80,11 @@ echo ""
 echo "Saving results to S3..."
 $AWS s3 sync $RESULTS_PATH $S3_RESULTS_PATH
 
-# If the local and existing environments are different, update the env on the bucket
-if [ $UPDATE_ENV -eq 1 ]; then
-    echo ""
-    echo "Zipping conda env and saving to bucket..."
-    tar -cf /home/ubuntu/conda-envs.tar -C $CONDA_PATH .
-    $AWS s3 cp /home/ubuntu/conda-envs.tar s3://$BUCKET_NAME/
-fi
-
 conda deactivate
 echo ""
 echo "Job finished."
 
 if [ "$DELETE_WHEN_DONE" = "yes" ]; then
-    echo "Deleting instance $1"
+    echo "Deleting instance $1..."
     $AWS ec2 terminate-instances --instance-ids "$1"
 fi
