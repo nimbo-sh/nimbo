abc21f1f4e37b775852065350b149a15b1eb8561
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-04-30 15:39:41 +0100

    flake: add dependencies and pin flake.lock

--- flake.nix
+++ flake.nix
@@ -1,11 +1,24 @@
-{
-  inputs = {
-    mach-nix.url = "github:DavHau/mach-nix?rev=3.2.0";
-  };
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-  let pkgs = nixpkgs.legacyPackages.${system};
-  {
-    devShell = pkgs.mkShell {
-    };
-  });
+
+  description = "Nimbo development shell";
+  nixConfig.bash-prompt = "[nimbo-dev-shell]$ ";
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            with python3Packages; [
+              python3
+
+              awscli
+              boto3
+              click
+              pydantic
+              pyyaml
+              requests
+              setuptools
+            ];
+        };
+      });
 }
