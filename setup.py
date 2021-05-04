39cb26bcfb9e7295577a7f115a49e40be520bfd8
seuqaj114 <migjaques@gmail.com>
2021-05-04 21:33:25 +0100

    v0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nimbo",  # Replace with your own username
-    version="0.2.6",
+    version="0.2.7",
     author="NimboSH, Ltd.",
     author_email="support@nimbo.sh",
     description="Run machine learning jobs on AWS with a single command.",
